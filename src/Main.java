babf3562766d4985126223a9b25a4f2d64bcaca0
Tooba Jawed <toobja@hotmail.com>
2023-07-18 18:08:05 +0100

    updating main

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,27 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Application to Create school Clubs");
-        // create a class
+        // create a classroom object for a new class
         Classroom reception = new Classroom("Reception", 1);
+        //create few students
         Student student1 = new Student("xyz", 5, reception);
         Student student2 = new Student("abc", 5, reception);
+        // increment the number of students in class
         reception.setClassNumberOfStudents(2);
+        //creat a school club
         SchoolClub footBallClub = new SchoolClub("Foot Ball Club");
+        //test the attributes are set correct by constructor
         System.out.println("Club Name:" + footBallClub.getSchoolClubName());
         System.out.println("Club Members:" + footBallClub.getClubMembers());
+        //add two students previously created in club
         footBallClub.setClubMembers(student1);
         footBallClub.setClubMembers(student2);
-        System.out.println("Club Members:" + footBallClub.getClubMembers());
+        // test all the students in the club
+        for (Student member : footBallClub.getClubMembers()) {
+            System.out.println("Club Members:" + member.getStudentName());
+
+        }
+
 
     }
 }
